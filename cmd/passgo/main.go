0de5e7a0ee82a524b8efd0346cb4f99bee7c1eab
Marek Honzal <hi@marekhonzal.com>
2026-06-03 23:26:31 +0200

    feat: server skeleton, project entrypoint

---
+++ cmd/passgo/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("Pass Go"))
+	})
+
+	log.Println("listening on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
